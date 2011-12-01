6a373c45be668db4661a4587dad5286390f23d42
jinhu <jin@hukelou.com>
2011-12-01 13:46:55 +0100

    empty rails frame

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FairyTales::Application.config.secret_token = '8b945eead88eacf34be86d64186a2d61ecbec91eb4c2d082de698b5c72a2d478b70163e801bfffbde28165dda628b32fa8619b355658ecb593e6529b65c7552a'
